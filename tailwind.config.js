38339bdb1afe8e75e322ebfe63dd89711a8b644a
vaibhav-dubey17 <dubeyvaibhav482@gmail.com>
2023-05-23 00:03:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish","sans-serif"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue300:"#2b84ea",
+        lightBlue500: "#0b72e7",
+        grayText:"#818597",
+        lighrGray: "#e2e2e2",
+        gratBlue:"#344a6c",
+        deepBlueHead:"#162f56",
+        gray2:"#525a76",
+
+      }
+    },
+  },
+  plugins: [],
+}
+
